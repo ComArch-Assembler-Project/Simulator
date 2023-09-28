11a79f640b601a1678e3f7c56bfdd2079860b27e
guita13921 <guita13921@gmail.com>
2023-09-28 16:12:43 +0700

    done iytpe beq add lw

--- simulator.java
+++ simulator.java
@@ -8,17 +8,22 @@ public class simulator {
     private static final int MAXLINELENGTH = 1000;
 
     public static class State {
-        int pc = 0;
+        int pc;
         int[] mem = new int[NUMMEMORY];
         int[] reg = new int[NUMREGS];
         int numMemory;
     }
+
     /*
          (address 0): 8454151 (hex 0x810007) lw 0 1 five :load reg1 with 5 (uses symbolic address)
+         //0000000(25:24)0100000010000000000000111
          (address 1): 9043971 (hex 0x8a0003)
+         //0000000       010 001 010 0000000000000011
          (address 2): 655361 (hex 0xa0001)
          (address 3): 16842754 (hex 0x1010002)
+         //0000000 100 000 001 0000000000000010
          (address 4): 16842749 (hex 0x100fffd)
+         //0000000 100 000 000 1111111111111101
          (address 5): 29360128 (hex 0x1c00000)
          (address 6): 25165824 (hex 0x1800000)
          (address 7): 5 (hex 0x5)
@@ -63,7 +68,7 @@ public class simulator {
             System.exit(1);
         }
 
-        //ตาต้าทำเอง
+        //ตาต้าทำเอง ไม่น่าได้ใช้ละ
         String binaryString = Integer.toBinaryString(state.mem[0]);
         int originalNumber = Integer.parseInt(binaryString, 2);
         int extractedBits = (originalNumber >> 21) & 0b111;
@@ -74,74 +79,75 @@ public class simulator {
             op_args[i] = (digit == '1') ? 1 : 0;
         }
         System.out.println("Original string: " + X);
-        System.out.println("Split array: " + op_args[0]+op_args[1]+op_args[2]);
+        System.out.println("Split array: " + op_args[0] + op_args[1] + op_args[2]);
 
         state.pc = 0;
-        int regA, regB, offset;
-        /*
-        int pc = state.pc;
-        int total = 0;
+        int regA, regB;
+        int offset = 0;
         int[] arg = new int[3];
-        int regA, regB, destReg;
-        for(int i = 1; i != 1; i++){
+        int total = 0;
+        for (int i = 0; i <= 6; i++) { //TEST 2 LOOP
             printState(state);
-            total++;
-            switch (mem[pc] >> 22){
+            switch (state.mem[state.pc] >> 22) {
                 case 0: //add 000
-                    rFormat(mem[pc], arg);
+                    rFormat(state.mem[state.pc], arg);
                     regA = state.reg[arg[0]];
                     regB = state.reg[arg[1]];
                     state.reg[arg[2]] = regA + regB;
                     break;
 
                 case 1://nand 001
-                    rFormat(mem[pc], arg);
-                    regA = state.reg[arg[0]];
-                    regB = state.reg[arg[1]];
-                    state.reg[arg[2]] = ~(regA & regB);
+                    //rFormat(mem[pc], arg);
+                    //regA = state.reg[arg[0]];
+                    //regB = state.reg[arg[1]];
+                    //state.reg[arg[2]] = ~(regA & regB);
                     break;
 
                 case 2://lw 010
-                    iFormat(mem[pc], arg);
-                    regA = state.reg[arg[0]];
-                    regB = state.reg[arg[1]];
-                    //add code here ไม่รู้ละทำต่อให้หน่อย
+                    // 0 = regA, 1 = regB, 2 = offsetField
+                    iFormat(state.mem[state.pc], arg);
+                    offset = arg[2] + state.reg[arg[0]];
+                    state.reg[arg[1]] = state.mem[offset];
                     break;
 
                 case 3://sw 011
-                    iFormat(mem[pc], arg);
-                    regA = state.reg[arg[0]];
-                    regB = state.reg[arg[1]];
+                    //iFormat(mem[pc], arg);
+                    //regA = state.reg[arg[0]];
+                    //regB = state.reg[arg[1]];
                     //add code here ไม่รู้ละทำต่อให้หน่อย
                     break;
 
                 case 4: //beq 100:
-                    iFormat(mem[pc], arg);
+                    iFormat(state.mem[state.pc], arg);
                     regA = state.reg[arg[0]];
                     regB = state.reg[arg[1]];
-                    //add code here ไม่รู้ละทำต่อให้หน่อย
-                    break;
-
-                case 5://jalr 101: เก็บค่า PC+1 ไว้ใน regB ซึ่ง PC คือ address ของ jalr instruction และกระโดดไปที่ address ที่ถูกเก็บไว้ใน regA แต่ถ้า regA และ regB คือ register ตัวเดียวกัน ให้เก็บ PC+1 ก่อน และค่อยกระโดดไปที่ PC+1
-                    jFormat(mem[pc], arg);
-                    regA = state.reg[arg[0]];
-                    regB = state.reg[arg[1]];
-                    if (regA = regB){
-                        pc++;
-                    }else {
-                        pc = regA;
+                    if( regA == regB ) {
+                        state.pc = state.pc + arg[2];
+                        // the +1 is handled by the regular increment.
                     }
                     break;
 
+                case 5://jalr 101: เก็บค่า PC+1 ไว้ใน regB ซึ่ง PC คือ address ของ jalr instruction และกระโดดไปที่ address ที่ถูกเก็บไว้ใน regA แต่ถ้า regA และ regB คือ register ตัวเดียวกัน ให้เก็บ PC+1 ก่อน และค่อยกระโดดไปที่ PC+1
+                    //jFormat(mem[pc], arg);
+                    //regA = state.reg[arg[0]];
+                    //regB = state.reg[arg[1]];
+                    //if (regA = regB){
+                    //    pc++;
+                    //}else {
+                    //    pc = regA;
+                    //}
+                    break;
+
                 case 6://bhalt : เพิ่มค่า PC เหมือน instructions อื่นๆ และ halt เครื่อง นั่นคือให้ simulator รู้ว่าเครื่องมีการ halted เกิดขึ้น
-                    pc++;
-                    i = -1;
+                    //pc++;
+                    //i = -1;
                     break;
 
                 case 7://noop
                     //ไม่ทำอะไร
                     break;
             }
+            state.pc++;
         }
     }
 
@@ -150,16 +156,24 @@ public class simulator {
         arg[1] = (bit & (7 << 16 )) >> 16; // regB เอา bit ที่ 19-17
         arg[2] = bit & 7; // destReg เอา bit ที่ 2-0
     }
-
     private static void iFormat(int bit, int[] arg){
         arg[0] = (bit & (7 << 19 )) >> 19; // regA เอา bit ที่ 22-20
         arg[1] = (bit & (7 << 16 )) >> 16; // regB เอา bit ที่ 19-17
-        // ไม่รู้ละทำต่อให้หน่อย
+        arg[2] = bit & 0xFFFF;
+        int temp = convertNum(arg[2]);
+        arg[2] = temp;
+        System.out.println("LOWER"+arg[0]+":"+arg[1]+":"+arg[2]);
+
     }
 
     private static void jFormat(int bit, int[] arg) {
         arg[0] = bit & 0x3FFFFFF; // regA เอา 22 bit แรก (0x3FFFFFF คือ 1 22ตัว)
     }
-         */
+
+    public static int convertNum(int num) {
+        if ((num & (1 << 15)) != 0) {
+            num -= (1 << 16);
+        }
+        return num;
     }
 }
